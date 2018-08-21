7e5021779cd0b0661d7635c4117b351acf84d759
WLA <wlambert@orok.solutions>
2018-08-21 08:43:17 +0200

    Initial commit
    Eclipse project

---
+++ wix_w40k/Model.py
@@ -0,0 +1,17 @@
+'''
+Created on 21 août 2018
+
+@author: WLA
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
